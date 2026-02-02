218bf2686c5f9c5acf899999e3920c9c45c88e2c
anuraj-ak <129406166+anuraj-ak@users.noreply.github.com>
2026-02-02 23:16:43 +0530

    Add files via upload

---
+++ day1/code5.java
@@ -0,0 +1,11 @@
+public class code5 {
+    public static void main(String[] args) {
+        //StringBuilder sc=new StringBuilder("anuraj");
+        StringBuilder sc=new StringBuilder("");
+        
+        for(char ch='a';ch<='z';ch++){
+         sc.append(ch);
+        }
+        System.out.println(sc);
+    }
+}
